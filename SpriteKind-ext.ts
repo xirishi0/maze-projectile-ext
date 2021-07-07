0b485ba47394bb3f9ac3fade86ec8df4a3967aa0
xirishi0 <86041651+xirishi0@users.noreply.github.com>
2021-07-07 17:46:15 +0800

    更新 SpriteKind-ext.ts

--- SpriteKind-ext.ts
+++ SpriteKind-ext.ts
@@ -1,12 +1,7 @@
 namespace SpriteKind{
-    //% isKind
     export const PlayerBullet = SpriteKind.create()
-    //% isKind
     export const PlayerWeapon = SpriteKind.create()
-    //% isKind
     export const PlayerServant = SpriteKind.create()
-    //% isKind
     export const EnemyBullet = SpriteKind.create()
-    //% isKind
     export const BOSS = SpriteKind.create()
 }
